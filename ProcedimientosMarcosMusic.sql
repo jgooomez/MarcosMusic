f95e256f13cc659f68bbc844cb6a0ff69b8f2d98
AlejandroDaniel <115980259+Surprimo@users.noreply.github.com>
2023-06-04 23:59:23 +0200

    Update ProcedimientosMarcosMusic.sql

--- ProcedimientosMarcosMusic.sql
+++ ProcedimientosMarcosMusic.sql
@@ -404,3 +404,68 @@ BEGIN
         PRINT 'El departamento no existe.';
     END
 END
+----Alejandro Daniel Pastor Diez----
+----FUNCIONES----
+--Funcion para comprobar si existe un departamento
+CREATE FUNCTION ComprobarExistenciaDepartamento
+    (@idDepartamento INT)
+RETURNS BIT
+AS
+BEGIN
+    DECLARE @existeDepartamento BIT;
+
+    IF EXISTS (SELECT * FROM Departamento WHERE idDepartamento = @idDepartamento)
+        SET @existeDepartamento = 1;
+    ELSE
+        SET @existeDepartamento = 0;
+
+    RETURN @existeDepartamento;
+END
+
+--PROCEDIMIENTOS--
+--Procedimiento para crear un departamento SOLO si no existe
+CREATE PROCEDURE CrearDepartamento
+    @idDepartamento INT,
+    @nombreDepartamento NVARCHAR(50),
+    @fechaCreacion DATE,
+    @nombreEncargado NVARCHAR(50),
+    @numTrabajadores INT,
+    @numSubdepartamentos INT
+AS
+BEGIN
+    -- Verificar si el departamento ya existe
+    IF EXISTS (SELECT 1 FROM Departamento WHERE idDepartamento = @idDepartamento)
+    BEGIN
+        PRINT 'El departamento ya existe.';
+    END
+    ELSE
+    BEGIN
+        -- Insertar el nuevo departamento
+        INSERT INTO Departamento (idDepartamento, nombre, fechaCreacion, NombreEncargado, numTrabajadores, numSubDpto)
+        VALUES (@idDepartamento, @nombreDepartamento, @fechaCreacion, @nombreEncargado, @numTrabajadores, @numSubdepartamentos);
+        PRINT 'El departamento ha sido creado.';
+    END
+END
+
+--Procedimiento para eliminar un departamento SOLO si existe
+CREATE PROCEDURE dbo.EliminarDepartamentoSiExiste
+    @idDepartamento INT
+AS
+BEGIN
+    DECLARE @existeDepartamento BIT;
+
+    -- Comprobar si el departamento existe
+    SET @existeDepartamento = dbo.ComprobarExistenciaDepartamento(@idDepartamento);
+
+    IF @existeDepartamento = 1
+    BEGIN
+        -- El departamento existe, proceder a eliminarlo
+        DELETE FROM Departamento WHERE idDepartamento = @idDepartamento;
+        PRINT 'El departamento ha sido eliminado.';
+    END
+    ELSE
+    BEGIN
+        -- El departamento no existe
+        PRINT 'El departamento no existe.';
+    END
+END
