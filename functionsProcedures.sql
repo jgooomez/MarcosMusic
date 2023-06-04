be70d35085487857dcfe07cf6db98b1e575a973d
homesickcookies <109850009+homesickcookies@users.noreply.github.com>
2023-06-04 10:54:47 +0200

    Update functionsProcedures.sql

--- functionsProcedures.sql
+++ functionsProcedures.sql
@@ -1,5 +1,6 @@
---FUNC
+"--FUNCIONES Y PROCEDURES PARA EMPLEADOS Y DEPARTAMENTOS--"
 
+--PROCEDIMIENTO QUE PERMITE ACTUALIZAR AL ENCARGADO DE DEPARTAMENTO--
 CREATE PROCEDURE ActualizarEncargado
     @idDepartamento int,
     @NuevoEncargado nvarchar(50)
@@ -10,7 +11,25 @@ BEGIN
     WHERE idDepartamento = @idDepartamento
 END
 
+--FUNCION PARA OPTENER UNA LISTA DE NOMBRES Y EDADES DE TODOS LOS EMPLEADOS DE UN DEPARTAMENTO--
+CREATE FUNCTION getEmpleados (@idDepartamento int)
+RETURNS TABLE
+AS
+RETURN
+(
+    SELECT idEmpleado AS ID, nombre as Nombre, edad AS Edad
+    FROM Empleado
+    WHERE idDepartamento = @idDepartamento
+)
 
-exec ActualizarEncargado
-	@idDepartamento = 1,
-	@NuevoEncargado = 'Daniel Pantoja'
+--FUNCION PARA VER LOS EMPLEADOS A LOS QUE LES QUEDAN 5 AÑOS O MENOS PARA JUBILARSE--
+CREATE FUNCTION EmpCercaJubilacion()
+RETURNS TABLE
+AS
+RETURN
+(
+	SELECT Emp.nombre, Emp.edad, Dep.nombre AS nombreDepartamento
+    FROM Empleado Emp
+    INNER JOIN Departamento Dep ON Emp.idDepartamento = Dep.idDepartamento
+    WHERE EMP.edad >= 60
+)
