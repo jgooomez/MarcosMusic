91c5b1e40a7027f59063e7b34604d977b6b6cb7c
wassim <115979743+wass3@users.noreply.github.com>
2023-06-05 00:00:30 +0200

    procedimientos y funciones wassim

--- ProcedimientosMarcosMusic.sql
+++ ProcedimientosMarcosMusic.sql
@@ -469,3 +469,367 @@ BEGIN
         PRINT 'El departamento no existe.';
     END
 END
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--	  Wassim Atiki 	  --
+--		P R O C E D I M I E N T O S		  --
+
+-- PROCEDIMIENTO 1			(Devuélve el número de usuarios que tienen X tipo de suscripción)
+CREATE PROCEDURE ObtenerUsuariosPorTipoSuscripcion
+    @tipoSuscripcion VARCHAR(100),
+    @numUsuarios INT OUTPUT
+AS
+BEGIN
+    SELECT @numUsuarios = COUNT(*) 
+    FROM SubscripcionUsuario su
+    INNER JOIN Subscripcion s ON su.idSubscripcion = s.id
+    WHERE s.tipo = @tipoSuscripcion;
+
+    -- Devolver el resultado
+    SELECT @numUsuarios AS 'CantidadUsuarios';
+END
+
+/*		para ejecutar el procedimiento
+DECLARE @numUsuarios INT;
+EXEC ObtenerUsuariosPorTipoSuscripcion 'premium', @numUsuarios OUTPUT;
+SELECT @numUsuarios AS CantidadUsuarios;
+*/
+
+
+
+-- PROCEDIMIENTO 2			(Muestra los conciertos que tiene un artista)
+CREATE PROCEDURE ObtenerConciertosPorArtista
+    @nombreArtista VARCHAR(100)
+AS
+BEGIN
+    SELECT c.*
+    FROM Concierto c
+    INNER JOIN ConciertoArtista ca ON c.codigo = ca.codigoConcierto
+    INNER JOIN Artista a ON ca.idArtista = a.id
+    WHERE a.nombre = @nombreArtista;
+END
+
+/*
+EXEC ObtenerConciertosPorArtista 'Nirvana';
+*/
+
+
+
+-- PROCEDIMIENTO 3			(Obtener el número total de reproducciones de un contenido)
+CREATE PROCEDURE ObtenerTotalReproduccionesContenido
+    @codigoContenido INT,
+    @totalReproducciones INT OUTPUT
+AS
+BEGIN
+    SELECT @totalReproducciones = SUM(numeroReproducciones)
+    FROM Contenido
+    WHERE codigo = @codigoContenido;
+
+    -- Devolver el resultado
+    SELECT @totalReproducciones AS 'TotalReproducciones';
+END
+
+/*
+DECLARE @totalReproducciones INT;
+EXEC ObtenerTotalReproduccionesContenido @codigoContenido = codigo_contenido, @totalReproducciones = @totalReproducciones OUTPUT;
+SELECT @totalReproducciones AS TotalReproducciones;
+*/
+
+
+
+-- PROCEDIMIENTO 4			(Obtener la lista de reproducción más popular)
+CREATE PROCEDURE ObtenerListaReproduccionMasPopular
+AS
+BEGIN
+    SELECT TOP 1 *
+    FROM ListaReproduccion
+    ORDER BY numSeguidores DESC;
+END
+
+/*
+EXEC ObtenerListaReproduccionMasPopular;
+*/
+
+
+
+-- PROCEDIMIENTO 5			(Obtener la lista de canciones de una lista de reproducción específica)
+CREATE PROCEDURE ObtenerCancionesListaReproduccion
+    @codigoLista INT
+AS
+BEGIN
+    SELECT c.*
+    FROM Contenido c
+    INNER JOIN ListaCanciones lc ON c.codigo = lc.codigoCancion
+    WHERE lc.codigoLista = @codigoLista;
+END
+
+/*
+EXEC ObtenerCancionesListaReproduccion @codigoLista = codigo_lista;
+*/
+
+
+
+-- PROCEDIMIENTO 6			(Obtener el número de premios ganados por un artista)
+CREATE PROCEDURE ObtenerNumeroPremiosArtista
+    @nombreArtista VARCHAR(100),
+    @numPremios INT OUTPUT
+AS
+BEGIN
+    SELECT @numPremios = numPremios
+    FROM Artista
+    WHERE nombre = @nombreArtista;
+
+    -- Devolver el resultado
+    SELECT @numPremios AS 'NumeroPremios';
+END
+
+/*
+DECLARE @numPremios INT;
+EXEC ObtenerNumeroPremiosArtista 'nombre_artista', @numPremios = @numPremios OUTPUT;
+SELECT @numPremios AS NumeroPremios;
+*/
+
+
+
+--PROCEDIMIENTO 7			(Obtener la lista de empleados de un departamento junto con la cantidad de subdepartamentos que tiene ese departamento)
+CREATE PROCEDURE ObtenerEmpleadosConSubdepartamentos
+    @nombreDepartamento NVARCHAR(50)
+AS
+BEGIN
+    SELECT e.*, d.numSubDpto
+    FROM Empleado e
+    INNER JOIN Departamento d ON e.departamento = d.nombre
+    WHERE d.nombre = @nombreDepartamento;
+END
+
+/*
+EXEC ObtenerEmpleadosConSubdepartamentos @nombreDepartamento = nombre_departamento;
+*/
+
+
+
+-- PROCEDIMIENTO 8			(Obtener la lista de usuarios que han reproducido un contenido específico y sus detalles de reproducción)
+CREATE PROCEDURE ObtenerDetallesReproduccionesContenido
+    @codigoContenido INT
+AS
+BEGIN
+    SELECT u.*, r.duracion, r.fechaReproduccion, r.hora, r.valoracion
+    FROM Usuario u
+    INNER JOIN Reproduccion r ON u.idUsuario = r.idUsuario
+    WHERE r.codigoContenido = @codigoContenido;
+END
+
+/*
+EXEC ObtenerDetallesReproduccionesContenido @codigoContenido = codigo_contenido;
+*/
+
+
+-- PROCEDIMIENTO 9			(Obtener la cantidad total de dinero recaudado en todos los conciertos de un país específico)
+CREATE PROCEDURE ObtenerTotalRecaudadoPorPais
+    @pais VARCHAR(100),
+    @totalRecaudado DECIMAL(10, 2) OUTPUT
+AS
+BEGIN
+    SELECT @totalRecaudado = SUM(dineroRecaudado)
+    FROM Concierto
+    WHERE pais = @pais;
+
+    -- Devolver el resultado
+    SELECT @totalRecaudado AS 'TotalRecaudado';
+END
+
+/*
+DECLARE @totalRecaudado DECIMAL(10, 2);
+EXEC ObtenerTotalRecaudadoPorPais 'nombre_pais', @totalRecaudado = @totalRecaudado OUTPUT;
+SELECT @totalRecaudado AS TotalRecaudado;
+*/
+
+
+-- PROCEDIMIENTO 10			(Obtener la lista de usuarios que han suscrito una subscripción específica y su información de tarjeta de crédito)
+CREATE PROCEDURE ObtenerUsuariosPorSubscripcion
+    @tipoSubscripcion VARCHAR(100)
+AS
+BEGIN
+    SELECT u.*, t.numeroTarjeta, t.tipo, t.nombreTitular, t.cvv, t.caducidad
+    FROM Usuario u
+    INNER JOIN SubscripcionUsuario su ON u.idUsuario = su.idUsuario
+    INNER JOIN Subscripcion s ON su.idSubscripcion = s.id
+    INNER JOIN Tarjeta t ON u.idUsuario = t.idUsuario
+    WHERE s.tipo = @tipoSubscripcion;
+END
+
+/*
+EXEC ObtenerUsuariosPorSubscripcion 'tipo_subscripcion';
+*/
+
+
+
+
+
+
+
+
+--		F U N C I O N E S		--
+
+-- FUNCIÓN 1			(Devuelve el número total de categorías)
+CREATE FUNCTION ObtenerNumeroCategorias()
+RETURNS INT
+AS
+BEGIN
+    DECLARE @numCategorias INT;
+    SELECT @numCategorias = COUNT(*) FROM Categoria;
+    RETURN @numCategorias;
+END
+
+
+-- FUNCIÓN 2			(Devuelve el artista con más premios)
+CREATE FUNCTION ObtenerArtistaMasPremiado()
+RETURNS VARCHAR(100)
+AS
+BEGIN
+    DECLARE @artistaMasPremiado VARCHAR(100);
+    SELECT TOP 1 @artistaMasPremiado = nombre FROM Artista ORDER BY numPremios DESC;
+    RETURN @artistaMasPremiado;
+END
+
+
+-- FUNCIÓN 3			(Devuelve el promedio de valoración de los contenidos)
+CREATE FUNCTION ObtenerPromedioValoracionContenido()
+RETURNS DECIMAL(3, 2)
+AS
+BEGIN
+    DECLARE @promedioValoracion DECIMAL(3, 2);
+    SELECT @promedioValoracion = AVG(valoracion) FROM Contenido;
+    RETURN @promedioValoracion;
+END
+
+
+-- FUNCIÓN 4			(Devuelve el número total de reproducciones de un usuario)
+CREATE FUNCTION ObtenerTotalReproduccionesUsuario(@idUsuario INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @totalReproducciones INT;
+    SELECT @totalReproducciones = SUM(numeroReproducciones) FROM Contenido WHERE codigo IN (SELECT codigoContenido FROM Reproduccion WHERE idUsuario = @idUsuario);
+    RETURN @totalReproducciones;
+END
+
+
+-- FUNCIÓN 5			(Devuelve la lista de reproducción con la duración más larga)
+CREATE FUNCTION ObtenerListaReproduccionMasLarga()
+RETURNS INT
+AS
+BEGIN
+    DECLARE @codigoLista INT;
+    SELECT TOP 1 @codigoLista = codigo FROM ListaReproduccion ORDER BY duracionTotal DESC;
+    RETURN @codigoLista;
+END
+
+
+-- FUNCIÓN 6			(Devuelve el número de conciertos en una ciudad específica)
+CREATE FUNCTION ObtenerConciertosEnCiudad(@ciudad VARCHAR(100))
+RETURNS INT
+AS
+BEGIN
+    DECLARE @numConciertos INT;
+    SELECT @numConciertos = COUNT(*) FROM Concierto WHERE ciudad = @ciudad;
+    RETURN @numConciertos;
+END
+
+
+-- FUNCIÓN 7			(Devuelve la edad promedio de los artistas)
+CREATE FUNCTION ObtenerEdadPromedioArtistas()
+RETURNS INT
+AS
+BEGIN
+    DECLARE @edadPromedio INT;
+    SELECT @edadPromedio = AVG(DATEDIFF(YEAR, fechaInicio, GETDATE())) FROM Artista;
+    RETURN @edadPromedio;
+END
+
+
+-- FUNCIÓN 8			(Devuelve el número total de tarjetas registradas por un usuario)
+CREATE FUNCTION ObtenerTotalTarjetasRegistradas(@idUsuario INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @totalTarjetas INT;
+    SELECT @totalTarjetas = COUNT(*) FROM Tarjeta WHERE idUsuario = @idUsuario;
+    RETURN @totalTarjetas;
+END
+
+
+-- FUNCIÓN 9			(Devuelve la duración total de una lista de reproducción específica)
+CREATE FUNCTION ObtenerDuracionTotalListaReproduccion(@codigoLista INT)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @duracionTotal INT;
+    SELECT @duracionTotal = duracionTotal FROM ListaReproduccion WHERE codigo = @codigoLista;
+    RETURN @duracionTotal;
+END
+
+
+-- FUNCIÓN 10			(Devuelve el número de artistas de una nacionalidad específica)
+CREATE FUNCTION ObtenerArtistasNacionalidad(@nacionalidad VARCHAR(100))
+RETURNS INT
+AS
+BEGIN
+    DECLARE @numArtistas INT;
+    SELECT @numArtistas = COUNT(*) FROM Artista WHERE nacionalidad = @nacionalidad;
+    RETURN @numArtistas;
+END
+
+
+/* PARA EJECUTAR LAS FUNCIONES */
+-- Ejecutar la función 1: ObtenerNumeroCategorias
+SELECT dbo.ObtenerNumeroCategorias() AS NumeroCategorias;
+
+-- Ejecutar la función 2: ObtenerArtistaMasPremiado
+SELECT dbo.ObtenerArtistaMasPremiado() AS ArtistaMasPremiado;
+
+-- Ejecutar la función 3: ObtenerPromedioValoracionContenido
+SELECT dbo.ObtenerPromedioValoracionContenido() AS PromedioValoracionContenido;
+
+-- Ejecutar la función 4: ObtenerTotalReproduccionesUsuario
+DECLARE @idUsuario INT;
+SET @idUsuario = 1; -- Reemplazar con el ID de usuario deseado
+SELECT dbo.ObtenerTotalReproduccionesUsuario(@idUsuario) AS TotalReproduccionesUsuario;
+
+-- Ejecutar la función 5: ObtenerListaReproduccionMasLarga
+SELECT dbo.ObtenerListaReproduccionMasLarga() AS ListaReproduccionMasLarga;
+
+-- Ejecutar la función 6: ObtenerConciertosEnCiudad
+DECLARE @ciudad VARCHAR(100);
+SET @ciudad = 'NombreCiudad'; -- Reemplazar con el nombre de la ciudad deseada
+SELECT dbo.ObtenerConciertosEnCiudad(@ciudad) AS NumConciertosEnCiudad;
+
+-- Ejecutar la función 7: ObtenerEdadPromedioArtistas
+SELECT dbo.ObtenerEdadPromedioArtistas() AS EdadPromedioArtistas;
+
+-- Ejecutar la función 8: ObtenerTotalTarjetasRegistradas
+DECLARE @idUsuario INT;
+SET @idUsuario = 1; -- Reemplazar con el ID de usuario deseado
+SELECT dbo.ObtenerTotalTarjetasRegistradas(@idUsuario) AS TotalTarjetasRegistradas;
+
+-- Ejecutar la función 9: ObtenerDuracionTotalListaReproduccion
+DECLARE @codigoLista INT;
+SET @codigoLista = 1; -- Reemplazar con el código de lista de reproducción deseado
+SELECT dbo.ObtenerDuracionTotalListaReproduccion(@codigoLista) AS DuracionTotalListaReproduccion;
+
+-- Ejecutar la función 10: ObtenerArtistasNacionalidad
+DECLARE @nacionalidad VARCHAR(100);
+SET @nacionalidad = 'NombreNacionalidad'; -- Reemplazar con el nombre de la nacionalidad deseada
+SELECT dbo.ObtenerArtistasNacionalidad(@nacionalidad) AS NumArtistasNacionalidad;
